1a5828e57c6d3a92d83a2688c44a6b4f809e3e50
Fabio Germann <fabio.germann@scalepad.com>
2023-01-23 09:58:42 -0500

    better config

--- internal/provider/detection_rule_resource.go
+++ internal/provider/detection_rule_resource.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
-	"strconv"
 	"terraform-provider-elastic-siem/internal/helpers"
 	"time"
 
@@ -37,88 +36,140 @@ type DetectionRuleResourceModel struct {
 	Id          types.String `tfsdk:"id"`
 }
 
+type ThreatItem struct {
+	Framework string `json:"framework,omitempty"`
+	Tactic    struct {
+		ID        string `json:"id,omitempty"`
+		Name      string `json:"name,omitempty"`
+		Reference string `json:"reference,omitempty"`
+	} `json:"tactic,omitempty"`
+	Technique []struct {
+		ID        string `json:"id,omitempty"`
+		Name      string `json:"name,omitempty"`
+		Reference string `json:"reference,omitempty"`
+	} `json:"technique,omitempty"`
+}
+
+type ExecutionHistoryItem struct {
+	LastExecution struct {
+		Date        time.Time `json:"date,omitempty"`
+		Status      string    `json:"status,omitempty"`
+		StatusOrder int       `json:"status_order,omitempty"`
+		Message     string    `json:"message,omitempty"`
+		Metrics     struct {
+			TotalSearchDurationMs     int `json:"total_search_duration_ms,omitempty"`
+			TotalIndexingDurationMs   int `json:"total_indexing_duration_ms,omitempty"`
+			TotalEnrichmentDurationMs int `json:"total_enrichment_duration_ms,omitempty"`
+		} `json:"metrics,omitempty"`
+	} `json:"last_execution,omitempty"`
+}
+
+type RiskScoreMapping struct {
+	Field    string `json:"field,omitempty"`
+	Operator string `json:"operator,omitempty"`
+	Value    string `json:"value,omitempty"`
+}
+
+type SeverityMapping struct {
+	Field    string `json:"field,omitempty"`
+	Value    string `json:"value,omitempty"`
+	Operator string `json:"operator,omitempty"`
+	Severity string `json:"severity,omitempty"`
+}
+
+type ActionItem struct {
+	Group  string `json:"group,omitempty"`
+	ID     string `json:"id,omitempty"`
+	Params struct {
+		Body string `json:"body,omitempty"`
+	} `json:"params,omitempty"`
+	ActionTypeID string `json:"action_type_id,omitempty"`
+}
+
+type MetaItem struct {
+	From             string `json:"from,omitempty"`
+	KibanaSiemAppURL string `json:"kibana_siem_app_url,omitempty"`
+}
+
 type DetectionRuleResponse struct {
-	CreatedAt           time.Time     `json:"created_at,omitempty"`
-	UpdatedAt           time.Time     `json:"updated_at,omitempty"`
-	CreatedBy           string        `json:"created_by,omitempty"`
-	Description         string        `json:"description,omitempty"`
-	Enabled             bool          `json:"enabled,omitempty"`
-	FalsePositives      []interface{} `json:"false_positives,omitempty"`
-	From                string        `json:"from,omitempty"`
-	ID                  string        `json:"id,omitempty"`
-	Immutable           bool          `json:"immutable,omitempty"`
-	Interval            string        `json:"interval,omitempty"`
-	RuleID              string        `json:"rule_id,omitempty"`
-	OutputIndex         string        `json:"output_index,omitempty"`
-	MaxSignals          int           `json:"max_signals,omitempty"`
-	RiskScore           int           `json:"risk_score,omitempty"`
-	Name                string        `json:"name,omitempty"`
-	References          []interface{} `json:"references,omitempty"`
-	Severity            string        `json:"severity,omitempty"`
-	UpdatedBy           string        `json:"updated_by,omitempty"`
-	Tags                []string      `json:"tags,omitempty"`
-	To                  string        `json:"to,omitempty"`
-	Type                string        `json:"type,omitempty"`
-	Threat              []interface{} `json:"threat,omitempty"`
-	Version             int           `json:"version,omitempty"`
-	Actions             []interface{} `json:"actions,omitempty"`
-	Filters             []interface{} `json:"filters,omitempty"`
-	Throttle            string        `json:"throttle,omitempty"`
-	Query               string        `json:"query,omitempty"`
-	Language            string        `json:"language,omitempty"`
-	RelatedIntegrations []interface{} `json:"related_integrations,omitempty"`
-	RequiredFields      []interface{} `json:"required_fields,omitempty"`
-	ExecutionSummary    []interface{} `json:"execution_summary,omitempty"`
-	Setup               string        `json:"setup,omitempty"`
+	Actions             []ActionItem         `json:"actions,omitempty"`
+	Author              []string             `json:"author,omitempty"`
+	CreatedAt           time.Time            `json:"created_at,omitempty"`
+	CreatedBy           string               `json:"created_by,omitempty"`
+	Description         string               `json:"description,omitempty"`
+	Enabled             bool                 `json:"enabled,omitempty"`
+	ExecutionSummary    ExecutionHistoryItem `json:"execution_summary,omitempty"`
+	FalsePositives      []interface{}        `json:"false_positives,omitempty"`
+	Filters             []interface{}        `json:"filters,omitempty"`
+	From                string               `json:"from,omitempty"`
+	ID                  string               `json:"id,omitempty"`
+	Immutable           bool                 `json:"immutable,omitempty"`
+	Interval            string               `json:"interval,omitempty"`
+	Language            string               `json:"language,omitempty"`
+	MaxSignals          int                  `json:"max_signals,omitempty"`
+	Meta                MetaItem             `json:"meta,omitempty"`
+	Name                string               `json:"name,omitempty"`
+	OutputIndex         string               `json:"output_index,omitempty"`
+	Query               string               `json:"query,omitempty"`
+	References          []interface{}        `json:"references,omitempty"`
+	RelatedIntegrations []interface{}        `json:"related_integrations,omitempty"`
+	RequiredFields      []interface{}        `json:"required_fields,omitempty"`
+	RiskScore           int                  `json:"risk_score,omitempty"`
+	RiskScoreMapping    []RiskScoreMapping   `json:"risk_score_mapping,omitempty"`
+	RuleID              string               `json:"rule_id,omitempty"`
+	Setup               string               `json:"setup,omitempty"`
+	Severity            string               `json:"severity,omitempty"`
+	SeverityMapping     []SeverityMapping    `json:"severity_mapping,omitempty"`
+	Tags                []string             `json:"tags,omitempty"`
+	Threat              []ThreatItem         `json:"threat,omitempty"`
+	Throttle            string               `json:"throttle,omitempty"`
+	To                  string               `json:"to,omitempty"`
+	Type                string               `json:"type,omitempty"`
+	UpdatedAt           time.Time            `json:"updated_at,omitempty"`
+	UpdatedBy           string               `json:"updated_by,omitempty"`
+	Version             int                  `json:"version,omitempty"`
 }
 
 type DetectionRule struct {
-	Description         string        `json:"description,omitempty"`
-	Enabled             bool          `json:"enabled,omitempty"`
-	FalsePositives      []interface{} `json:"false_positives,omitempty"`
-	From                string        `json:"from,omitempty"`
-	ID                  string        `json:"id,omitempty"`
-	Immutable           bool          `json:"immutable,omitempty"`
-	Interval            string        `json:"interval,omitempty"`
-	RuleID              string        `json:"rule_id,omitempty"`
-	OutputIndex         string        `json:"output_index,omitempty"`
-	MaxSignals          int           `json:"max_signals,omitempty"`
-	RiskScore           int           `json:"risk_score,omitempty"`
-	Name                string        `json:"name,omitempty"`
-	References          []interface{} `json:"references,omitempty"`
-	Severity            string        `json:"severity,omitempty"`
-	UpdatedBy           string        `json:"updated_by,omitempty"`
-	Tags                []string      `json:"tags,omitempty"`
-	To                  string        `json:"to,omitempty"`
-	Type                string        `json:"type,omitempty"`
-	Threat              []interface{} `json:"threat,omitempty"`
-	Version             int           `json:"version,omitempty"`
-	Actions             []interface{} `json:"actions,omitempty"`
-	Filters             []interface{} `json:"filters,omitempty"`
-	Throttle            string        `json:"throttle,omitempty"`
-	Query               string        `json:"query,omitempty"`
-	Language            string        `json:"language,omitempty"`
-	RelatedIntegrations []interface{} `json:"related_integrations,omitempty"`
-	RequiredFields      []interface{} `json:"required_fields,omitempty"`
-	ExecutionSummary    []interface{} `json:"execution_summary,omitempty"`
-	Setup               string        `json:"setup,omitempty"`
+	Actions             []ActionItem       `json:"actions,omitempty"`
+	Author              []string           `json:"author,omitempty"`
+	Description         string             `json:"description,omitempty"`
+	Enabled             bool               `json:"enabled,omitempty"`
+	ExecutionSummary    []interface{}      `json:"execution_summary,omitempty"`
+	FalsePositives      []interface{}      `json:"false_positives,omitempty"`
+	Filters             []interface{}      `json:"filters,omitempty"`
+	From                string             `json:"from,omitempty"`
+	ID                  string             `json:"id,omitempty"`
+	Immutable           bool               `json:"immutable,omitempty"`
+	Interval            string             `json:"interval,omitempty"`
+	Language            string             `json:"language,omitempty"`
+	MaxSignals          int                `json:"max_signals,omitempty"`
+	Name                string             `json:"name,omitempty"`
+	OutputIndex         string             `json:"output_index,omitempty"`
+	Query               string             `json:"query,omitempty"`
+	References          []interface{}      `json:"references,omitempty"`
+	RelatedIntegrations []interface{}      `json:"related_integrations,omitempty"`
+	RequiredFields      []interface{}      `json:"required_fields,omitempty"`
+	RiskScore           int                `json:"risk_score,omitempty"`
+	RiskScoreMapping    []RiskScoreMapping `json:"risk_score_mapping,omitempty"`
+	RuleID              string             `json:"rule_id,omitempty"`
+	Setup               string             `json:"setup,omitempty"`
+	Severity            string             `json:"severity,omitempty"`
+	SeverityMapping     []SeverityMapping  `json:"severity_mapping,omitempty"`
+	Tags                []string           `json:"tags,omitempty"`
+	Threat              []ThreatItem       `json:"threat,omitempty"`
+	Throttle            string             `json:"throttle,omitempty"`
+	To                  string             `json:"to,omitempty"`
+	Type                string             `json:"type,omitempty"`
+	UpdatedBy           string             `json:"updated_by,omitempty"`
+	Version             int                `json:"version,omitempty"`
 }
 
 func extractRuleFronJSONStrging(ctx context.Context, yamlString string) (*DetectionRule, error) {
 	result := DetectionRule{}
 	tflog.Debug(ctx, yamlString)
-	s, err := strconv.Unquote(yamlString)
-	if err != nil {
-		tflog.Error(ctx, "Error in extractRuleFronJSONStrging (0)")
-		return nil, err
-	}
-	err = json.Unmarshal([]byte(s), &result)
-	if err != nil {
-		tflog.Error(ctx, "Error in extractRuleFronJSONStrging (1)")
-		return nil, err
-	}
-	tflog.Debug(ctx, result.Name)
-	return &result, nil
+	err := json.Unmarshal([]byte(yamlString), &result)
+	return &result, err
 }
 
 func (r *DetectionRuleResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
@@ -178,7 +229,7 @@ func (r *DetectionRuleResource) Create(ctx context.Context, req resource.CreateR
 	}
 
 	// Process the rule content
-	body, err := extractRuleFronJSONStrging(ctx, data.RuleContent.String())
+	body, err := extractRuleFronJSONStrging(ctx, data.RuleContent.ValueString())
 	if err != nil {
 		resp.Diagnostics.AddError("Parser Error", fmt.Sprintf("Unable to parse file, got error: %s", err))
 		return
@@ -210,7 +261,7 @@ func (r *DetectionRuleResource) Read(ctx context.Context, req resource.ReadReque
 
 	// Get the rule through the API
 	var response DetectionRuleResponse
-	path := fmt.Sprintf("/detection_engine/rules?id=%s", data.Id)
+	path := fmt.Sprintf("/detection_engine/rules?id=%s", data.Id.ValueString())
 	if err := r.client.Get(path, &response); err != nil {
 		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Error during request, got error: %s", err))
 		return
@@ -232,13 +283,13 @@ func (r *DetectionRuleResource) Update(ctx context.Context, req resource.UpdateR
 	}
 
 	// Process the rule content
-	body, err := extractRuleFronJSONStrging(ctx, data.RuleContent.String())
+	body, err := extractRuleFronJSONStrging(ctx, data.RuleContent.ValueString())
 	if err != nil {
 		resp.Diagnostics.AddError("Parser Error", fmt.Sprintf("Unable to parse file, got error: %s", err))
 		return
 	}
 
-	body.ID = data.Id.String()
+	body.ID = data.Id.ValueString()
 
 	// Create the rule through API
 	var response DetectionRuleResponse
@@ -262,7 +313,7 @@ func (r *DetectionRuleResource) Delete(ctx context.Context, req resource.DeleteR
 	}
 
 	// Get the rule through the API
-	path := fmt.Sprintf("/detection_engine/rules?id=%s", data.Id)
+	path := fmt.Sprintf("/detection_engine/rules?id=%s", data.Id.ValueString())
 	if err := r.client.Delete(path); err != nil {
 		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Error during request, got error: %s", err))
 		return
